d01d45cf0c0f87c53ac4faf4a266c26d98c066d8
IVAN KULIASHOV <42475953+Krivetka@users.noreply.github.com>
2021-03-01 18:57:53 +0300

    feat: add sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig){
+    str_brackets = str.split("");
+    arr=[];
+    for(i=0; i < str_brackets.length; i++){
+        for(a=0; a < bracketsConfig.length; a++){
+              if(str_brackets[i] == bracketsConfig[a][0]){
+                if(bracketsConfig[a][0]==bracketsConfig[a][1] && arr[arr.length-1] == a){
+                  arr.splice(arr.length-1);
+                }
+                else  arr.push(a);
+              }
+              else if(str_brackets[i] == bracketsConfig[a][1]) {
+                if(arr[arr.length-1] == a){
+                  arr.splice(arr.length-1);
+                }
+                else  arr.push(a);
+              }
+        }
+    }
+     console.log(arr);
+    if(arr.length==0) return true;
+    else return false;
+ }
